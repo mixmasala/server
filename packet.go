e2d8ed2c36252af14d0b51dd30915f7bfce9f3f8
Yawning Angel <yawning@schwanenlied.me>
2017-10-31 22:52:19 +0000

    mix: Clarify a comment, no functional changes.

--- packet.go
+++ packet.go
@@ -124,7 +124,7 @@ func (pkt *packet) copyToRaw(b []byte) error {
 	}
 
 	// The common case of standard packet sizes uses a pool allocator
-	// to store the packet payloads.
+	// to store the raw packets.
 	pkt.raw = rawPacketPool.Get().([]byte)
 
 	// Sanity check, just in case the pool allocator is doing something dumb.
