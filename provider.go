ddb2620994ad3c5f1be6372eeb4102c20fd3332a
Yawning Angel <yawning@schwanenlied.me>
2017-10-31 22:22:15 +0000

    provider: Finish implementing inbound packet processing.
    This still needs to initialize a concrete message spool implementation,
    but apart from that it will store packets in each user's spool, and
    generate SURB-ACKs as needed.

--- provider.go
+++ provider.go
@@ -21,7 +21,11 @@ import (
 	"sync"
 
 	"github.com/eapache/channels"
+	"github.com/katzenpost/core/constants"
+	"github.com/katzenpost/core/sphinx"
+	"github.com/katzenpost/core/sphinx/commands"
 	"github.com/katzenpost/core/wire"
+	"github.com/katzenpost/server/spool"
 	"github.com/katzenpost/server/userdb"
 	"github.com/katzenpost/server/userdb/boltuserdb"
 	"github.com/op/go-logging"
@@ -33,6 +37,7 @@ type provider struct {
 	s      *Server
 	ch     *channels.InfiniteChannel
 	userDB userdb.UserDB
+	spool  spool.Spool
 	log    *logging.Logger
 
 	haltCh chan interface{}
@@ -47,12 +52,13 @@ func (p *provider) halt() {
 		p.userDB.Close()
 		p.userDB = nil
 	}
+	if p.spool != nil {
+		p.spool.Close()
+		p.spool = nil
+	}
 }
 
 func (p *provider) authenticateClient(c *wire.PeerCredentials) bool {
-	p.Add(1)
-	defer p.Done()
-
 	isValid := p.userDB.IsValid(c.AdditionalData, c.PublicKey)
 	p.log.Debugf("Auth: User: '%v', Key: '%v': %v", asciiBytesToPrintString(c.AdditionalData), ecdhToPrintString(c.PublicKey), isValid)
 	return isValid
@@ -84,14 +90,110 @@ func (p *provider) worker() {
 		recipient := bytes.TrimRight(pkt.recipient.ID[:], "\x00")
 
 		// Ensure the packet is for a valid recipient.
+		//
+		// TODO/perf: This might be better off in the crypto worker since the
+		// db calls can be concurrent by virtue of being a read operation.
 		if !p.userDB.Exists(recipient) {
 			p.log.Debugf("Dropping packet: %v (Invalid Recipient: '%v')", pkt.id, asciiBytesToPrintString(recipient))
 			pkt.dispose()
 			continue
 		}
 
-		// XXX/provider: Implement the rest.
-		panic("BUG: Provider inbound packet processing is incomplete")
+		// All of the store operations involve writing to the database which
+		// won't really benefit from concurrency.
+		if pkt.isSURBReply() {
+			p.onSURBReply(pkt, recipient)
+		} else {
+			// Caller checks that the packet is either a SURBReply or a user
+			// message, so this must be the latter.
+			p.onToUser(pkt, recipient)
+		}
+
+		pkt.dispose()
+	}
+}
+
+func (p *provider) onSURBReply(pkt *packet, recipient []byte) {
+	// Store the payload in the spool.
+	if err := p.spool.StoreSURBReply(recipient, &pkt.surbReply.ID, pkt.payload); err != nil {
+		p.log.Debugf("Failed to store SURBReply: %v (%v)", pkt.id, err)
+	} else {
+		p.log.Debugf("Stored SURBReply: %v", pkt.id)
+	}
+}
+
+func (p *provider) onToUser(pkt *packet, recipient []byte) {
+	const (
+		hdrLength    = constants.SphinxPlaintextHeaderLength + sphinx.SURBLength
+		flagsPadding = 0
+		flagsSURB    = 1
+		reserved     = 0
+	)
+
+	// Parse the payload, which should be a valid BlockSphinxPlaintext.
+	b := pkt.payload
+	if len(b) < hdrLength {
+		p.log.Debugf("Dropping packet: %v (Truncated message block)", pkt.id)
+		return
+	}
+	if b[1] != reserved {
+		p.log.Debugf("Dropping packet: %v (Invalid message reserved: 0x%02x)", pkt.id, b[1])
+		return
+	}
+	ct := b[hdrLength:]
+	var surb []byte
+	switch b[0] {
+	case flagsPadding:
+	case flagsSURB:
+		surb = b[2:hdrLength]
+	default:
+		p.log.Debugf("Dropping packet: %v (Invalid message flags: 0x%02x)", pkt.id, b[0])
+		return
+	}
+
+	// Store the ciphertext in the spool.
+	if err := p.spool.StoreMessage(recipient, ct); err != nil {
+		p.log.Debugf("Failed to store message payload: %v (%v)", pkt.id, err)
+		return
+	}
+
+	// Iff there is a SURB, generate a SURB-ACK, and schedule.
+	if surb != nil {
+		// Build the SURB-ACK from the SURB.
+		//
+		// TODO/perf: This is a crypto operation and can be made concurrent,
+		// the logical place for this is probably the crypto workers.
+		var ackPayload [constants.ForwardPayloadLength]byte
+		rawAckPkt, firstHop, err := sphinx.NewPacketFromSURB(surb, ackPayload[:])
+		if err != nil {
+			p.log.Debugf("Failed to generate SURB-ACK: %v (%v)", pkt.id, err)
+			return
+		}
+
+		// Build the packet structure for the SURB-ACK.
+		ackPkt := newPacket()
+		ackPkt.copyToRaw(rawAckPkt)
+		ackPkt.cmds = make([]commands.RoutingCommand, 0, 2)
+
+		nextHopCmd := new(commands.NextNodeHop)
+		copy(nextHopCmd.ID[:], firstHop[:])
+		ackPkt.cmds = append(ackPkt.cmds, nextHopCmd)
+		ackPkt.nextNodeHop = nextHopCmd
+
+		nodeDelayCmd := new(commands.NodeDelay)
+		nodeDelayCmd.Delay = pkt.nodeDelay.Delay
+		ackPkt.cmds = append(ackPkt.cmds, nodeDelayCmd)
+		ackPkt.nodeDelay = nodeDelayCmd
+
+		ackPkt.recvAt = pkt.recvAt
+		ackPkt.delay = pkt.delay
+		ackPkt.mustForward = true
+
+		// Send the SURB-ACK off to the scheduler.
+		p.log.Debugf("Handing off user destined SURB-ACK: %v (Src:%v)", ackPkt.id, pkt.id)
+		p.s.scheduler.onPacket(ackPkt)
+	} else {
+		p.log.Debugf("Stored Message: %v (No SURB)", pkt.id)
 	}
 }
 
@@ -107,6 +209,9 @@ func newProvider(s *Server) (*provider, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// XXX/provider: Initialize the spool.
+	panic("BUG: Message spool initialization not done yet.")
 
 	p.Add(1)
 	go p.worker()
